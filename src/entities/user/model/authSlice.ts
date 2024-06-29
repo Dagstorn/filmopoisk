2563608ed1c82319cbff786601fff9602f14f06f
Alisher Abylkassimov <abylkassimov.alisher@gmail.com>
2024-06-29 12:19:52 +0500

    SPA ready

--- src/entities/user/model/authSlice.ts
+++ src/entities/user/model/authSlice.ts
@@ -34,5 +34,5 @@ const authSlice = createSlice({
   },
 });
 
-export const { setToken, clearToken,restoreLogin } = authSlice.actions;
+export const { setToken, clearToken, restoreLogin } = authSlice.actions;
 export default authSlice.reducer;
